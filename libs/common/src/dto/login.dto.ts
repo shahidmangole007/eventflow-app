2193eb86c28d9f7fa624d7cdf19064f0f62e3d89
shahid munir mangole <shahidsofficial07@gmail.com>
2026-04-14 10:59:09 +0530

    tsconfig Change

--- libs/common/src/dto/login.dto.ts
+++ libs/common/src/dto/login.dto.ts
@@ -4,9 +4,9 @@ export class LoginDto{
 
     @IsEmail({}, { message: 'Please provide a valid email address'})
     @IsNotEmpty({ message: 'Email is required'})
-    email: string;
+    email!: string;
 
     @IsString({ message: 'Password must be a string'})
     @IsNotEmpty({ message: 'Password is required'}) 
-    password: string;
+    password!: string;
 }
--- libs/common/src/dto/purchase-ticket.dto.ts
+++ libs/common/src/dto/purchase-ticket.dto.ts
@@ -6,18 +6,18 @@ export class PurchaseTicketDto {
 
     @IsUUID(4, { message: "eventId must be a valid UUID v4" })
     @IsNotEmpty({ message: "eventId is required" })
-    eventId: string;
+    eventId!: string;
 
     @IsInt({ message: "quantity must be an integer" })
     @Max(10, { message: "quantity cannot exceed 10" })
     @Min(1, { message: "quantity must be at least 1" })
     @IsNotEmpty({ message: "quantity is required" })
-    quantity: number;
+    quantity!: number;
 
 
     @IsNotEmpty({ message: "status is required" })
     @IsEnum(['PENDING', 'CONFIRMED', 'CHECKED_IN', 'CANCELED'], { message: "status must be one of: PENDING, CONFIRMED, CHECKED_IN, CANCELED" })
-    status: string;
+    status!: string;
 
 
 
