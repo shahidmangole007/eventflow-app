db7d8a63db3970399663f598a70dae7e7fe9a4d8
shahid munir mangole <shahidsofficial07@gmail.com>
2026-04-14 11:03:54 +0530

    purchase dto commented code remove

--- libs/common/src/dto/purchase-ticket.dto.ts
+++ libs/common/src/dto/purchase-ticket.dto.ts
@@ -24,21 +24,5 @@ export class PurchaseTicketDto {
 
 
 
-    // id: uuid('id').primaryKey().defaultRandom(),
-    // eventId: uuid('event_id')
-    //     .references(() => events.id)
-    //     .notNull(),
-    // userId: uuid('user_id')
-    //     .references(() => users.id)
-    //     .notNull(),
-    // quantity: integer('quantity').default(1).notNull(),
-    // totalPrice: integer('total_price').notNull(),
-    // status: ticketStatusEnum('status').default('PENDING').notNull(),
-    // ticketCode: varchar('ticket_code', { length: 20 }).notNull().unique(),
-    // purchasedAt: timestamp('purchased_at').defaultNow().notNull(),
-    // checkedInAt: timestamp('checked_in_at'),
-    // createdAt: timestamp('created_at').notNull().defaultNow(),
-    // updatedAt: timestamp('updated_at').notNull().defaultNow()
-
 
 }
